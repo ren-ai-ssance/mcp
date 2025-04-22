1154609d02b1f745eb16e6b37f7f6cb8ab29ead5
John Park <kyopark2014@gmail.com>
2025-04-22 16:38:52 +0900

    docker

--- cdk-mcp-rag/lib/cdk-mcp-rag-stack.ts
+++ cdk-mcp-rag/lib/cdk-mcp-rag-stack.ts
@@ -451,7 +451,7 @@ export class CdkMcpRagStack extends cdk.Stack {
     );
 
     const targetPort = 8501;  // 8080 8501
-    // ec2Sg.connections.allowFrom(albSg, ec2.Port.tcp(targetPort), 'allow traffic from alb') // alb -> ec2
+    ec2Sg.connections.allowFrom(albSg, ec2.Port.tcp(targetPort), 'allow traffic from alb') // alb -> ec2
     ec2Sg.connections.allowTo(bedrockEndpoint, ec2.Port.tcp(443), 'allow traffic to bedrock endpoint') // ec2 -> bedrock
 
     // cloudfront for sharing s3
@@ -631,7 +631,7 @@ EOF"`,
     targets.push(new elbv2_tg.InstanceTarget(appInstance)); 
     
     // ALB Listener
-  /*  const listener = alb.addListener(`HttpListener-for-${projectName}`, {   
+    const listener = alb.addListener(`HttpListener-for-${projectName}`, {   
       port: 80,
       open: true
     });     
@@ -652,6 +652,6 @@ EOF"`,
     })
     listener.addAction(`RedirectHttpListener-for-${projectName}`, {
       action: defaultAction
-    });    */
+    });    
   }
 }
